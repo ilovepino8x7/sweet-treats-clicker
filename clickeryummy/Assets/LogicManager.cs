4c067199ca871bb6109254bf9b42e85f18382f1c
ilovepino8x7 <teddybhunt@gmail.com>
2025-12-22 19:28:43 +0000

    Added UI of shop and upgrade functionality
    I made UI for the shop, imported a font, made upgrade functionality for the mouse and made a new currency

--- clickeryummy/Assets/LogicManager.cs
+++ clickeryummy/Assets/LogicManager.cs
@@ -1,22 +1,46 @@
 using UnityEngine;
 using TMPro;
+using System.ComponentModel;
+using System;
 public class LogicManager : MonoBehaviour
 {
     [HideInInspector]
     public int clicks = 0;
     public TMP_Text clickText;
     public GameObject mouse;
+    [HideInInspector]
+    public int clickPower;          //0, 1, 2, 3, 4, 5;
+    //private int[] spoonStrength = { 1, 10, 250, 1000, 10000, 100000};
+    // wooden, bronze, silver, gold, diamond, platinum;
+    private int[] prices = { 250, 10000, 1000000, 10000000, 1000000000};
+    [HideInInspector]
+    public int money;
+    public TMP_Text moneyCount;
+    private int currentLevel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        clickPower = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         clickText.text = clicks.ToString() + " Clicks";
+        moneyCount.text = money.ToString() + "x";
         Cursor.visible = false;
         mouse.transform.position = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+    }
+    public void Upgrade()
+    {
+        if (clickPower >= prices.Length)
+        {
+            return;
+        }
+        if (money >= prices[clickPower])
+        {
+            money -= prices[clickPower];
+            clickPower++;
+        }
     }
 }
--- clickeryummy/Assets/tirimasuControl.cs
+++ clickeryummy/Assets/tirimasuControl.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class fudgeControl : MonoBehaviour
 {
     public LogicManager ls;
+    // wooden, bronze, silver, gold, diamond, platinum
+    //sharperspoon
+    private int[] spoonStrength = { 1, 10, 250, 1000, 10000, 100000,};
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +19,7 @@ public class fudgeControl : MonoBehaviour
     }
     void OnMouseDown()
     {
-        ls.clicks++;
+        ls.clicks += spoonStrength[ls.clickPower];
+        ls.money += spoonStrength[ls.clickPower];
     }
 }
